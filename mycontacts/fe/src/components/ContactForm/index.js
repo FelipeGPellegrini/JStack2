454167c4132403d631337b8bdb96a1e0bcaae11e
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-04-20 11:29:56 -0300

    formatação phone

--- mycontacts/fe/src/components/ContactForm/index.js
+++ mycontacts/fe/src/components/ContactForm/index.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
 
+
 import { Form, ButtonContainer } from "./styles"
+import useErrors from '../../hooks/useErrors'
 import FormGroup from "../FormGroup"
 import Input from "../Input"
 import Select from "../Select"
 import Button from "../Button"
+import isEmailValid from '../../utils/isEmailValid'
+import formatPhone from '../../utils/formatPhone'
 import { useState } from 'react'
 
 export default function ContactForm({ buttonLabel }) {
@@ -13,44 +17,52 @@ export default function ContactForm({ buttonLabel }) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
-  const [errors, setErrors] = useState([]);
+
+  const { setError, removeError, getErrorMessageByFieldName, errors } = useErrors();
+
+  const isFormValid = (name && errors.length === 0);
 
   function handleNameChange(event) {
     setName(event.target.value)
 
     if (!event.target.value) {
-      setErrors((prevState) => [
-        ...prevState,
-        { field: 'name', message: 'Nome é obrigatório' }
-      ])
+      setError({ field: 'name', message: 'Nome é obrigatório' })
     } else {
-      setErrors((prevState) => prevState.filter(
-        (error) => error.field !== 'name',
-      ))
+      removeError('name')
     }
 
-
-
   }
-  console.log(errors)
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      setError({ field: 'email', message: 'E-mail inválido.' })
+    } else {
+      removeError('email')
+    }
+  }
+
+  function handlePhoneChange(event) {
+    setPhone(formatPhone(event.target.value));
+  }
+
   function handleSubmit(event) {
     event.preventDefault()
     console.log({
       name, email, phone, category
     })
   }
-
-
   return (
-    <Form onSubmit={handleSubmit}>
-      <FormGroup >
-        <Input placeholder="Nome" value={name} onChange={handleNameChange} />
+    <Form onSubmit={handleSubmit} noValidate>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input error={getErrorMessageByFieldName('name')} placeholder="Nome *" value={name} onChange={handleNameChange} />
       </FormGroup>
-      <FormGroup error="O formato do e-mail é inválido">
-        <Input placeholder="E-mail" value={email} onChange={(event) => setEmail(event.target.value)} error />
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input type="email" placeholder="E-mail" value={email} onChange={handleEmailChange} error={getErrorMessageByFieldName('email')} />
       </FormGroup>
       <FormGroup >
-        <Input placeholder="Telefone" value={phone} onChange={(event) => setPhone(event.target.value)} />
+        <Input placeholder="Telefone" value={phone} onChange={handlePhoneChange} maxLength="15" />
       </FormGroup>
       <FormGroup >
         <Select value={category} onChange={(event) => setCategory(event.target.value)}>
@@ -61,7 +73,7 @@ export default function ContactForm({ buttonLabel }) {
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="submit">
+        <Button type="submit" disabled={!isFormValid}>
           {buttonLabel}
         </Button>
       </ButtonContainer>
